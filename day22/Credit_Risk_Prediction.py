d5754cc23ec88c9a417f01bcd61d086bc0171813
zsoda3582-cmd <zsoda3582@gmail.com>
2026-04-09 14:46:42 +0800

    Credit_Risk_Prediction

--- day22/Credit_Risk_Prediction.py
+++ day22/Credit_Risk_Prediction.py
@@ -1,4 +1,5 @@
 import pandas as pd
+#读数据
 df = pd.read_csv("loan_data.csv")
 df.info()
 print("--------------------------------------------------------------------")
@@ -101,3 +102,53 @@ best_t = thresholds[best_idx]
 print("best_threshold",best_t)
 print("best_precision",precision_arr[best_idx])
 print("best_recall",recall_arr[best_idx])
+print("--------------------------------------------------------------------")
+
+base_customer = x_test.iloc[[0]].copy() #我从测试集中选择了第一个客户（第0行），将其复制下来，返回到base_customer这个二维表格中
+print(base_customer)
+print("--------------------------------------------------------------------")
+
+#研究这个人，如果只把他的收入改掉，系统对于这个人的判断会怎么变？
+# base_prob1 = model.predict_proba(base_customer)           #[[0.9942474  0.00575264]]
+# base_prob2 = model.predict_proba(base_customer)[:,1]      #[0.00575264]
+base_prob3 = model.predict_proba(base_customer)[:,1][0]   #0.00575264
+#base_prob1->3 :二维表 → 一维数组 → 单个数值
+
+# temp_customer = base_customer.copy()
+# print("原来的收入：",temp_customer["person_income"].values[0]) #原来收入是84973.0
+# #改收入 & 收入贷款比
+# temp_customer["person_income"] = 20000
+# loan_amount = temp_customer["loan_amnt"].values[0]
+# temp_customer["loan_percent_income"] = loan_amount/20000
+# #评估
+# new_prob = model.predict_proba(temp_customer)[:,1][0]
+# print(new_prob)
+
+income_List = [80000,70000,60000,50000,40000,30000,20000]
+prob_List = []
+for income in income_List:
+    #copy 改income 改收入贷款比 评估
+    temp_customer = base_customer.copy()
+    temp_customer["person_income"] = income
+    loan_amount = temp_customer["loan_amnt"].values[0]
+    temp_customer["loan_percent_income"] = loan_amount/income
+    temp_prob = model.predict_proba(temp_customer)[:,1][0]
+    prob_List.append(temp_prob)
+print(prob_List)
+
+import matplotlib.pyplot as plt
+plt.figure(figsize=(8,5)) #控制图的大小，宽8，高5（英寸）
+plt.plot(income_List,prob_List,marker = "o") #在每个数据点上画一个“圆点”
+plt.title("Income vs Default_probably")
+plt.xlabel("Income")
+plt.ylabel("Default_probably")
+plt.grid()
+plt.show()
+
+
+"""
+1.当保持其他特征基本不变，并同步调整 loan_percent_income 后，模型对收入变化的反应并不是完全平滑的，但整体上，当收入降到较低水平时，预测违约概率会明显升高，
+特别是在收入降到 20000 时，风险急剧上升
+
+2.代码107-146行：what-if 分析 = 如果我改变一个变量，会发生什么？
+"""
